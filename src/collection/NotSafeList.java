f111ea2e4bf6daf70e262371655636943a782f4a
yn <hisa905395@gmail.com>
2025-02-03 00:56:32 +0800

    add not safe list example

---
+++ src/collection/NotSafeList.java
@@ -0,0 +1,37 @@
+package collection;
+
+// under multithreaded env, all collections are not safe(List, Map, etc)
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotSafeList {
+    public static void main(String[] args) {
+        List<String> list  = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                list.add(Thread.currentThread().getName());
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+}
+
+// ConcurrentModificationException
+// reason: the "add" method is not locked
+
+// solution 1: use Vector which add method has synchronized keyword, but Vector very slow
+// solution 2: use Collections.synchronizedList(new ArrayList())
+// solution 3: use CopyOnWriteArrayList
+//      thread copy the resources when writing something to it, this is a concept of
+//      read/write separation, the "add" method uses ReentrantLock, and use
+//      Arrays.copyOf() to copy the original resource, and write new value to the
+//      new copy
+
+
+
+
+
+
+
+
